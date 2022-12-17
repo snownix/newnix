0c690b336ceca1a609955822ab0b432160f793e5
yassinrais <yassmailrais@gmail.com>
2022-12-17 23:10:08 +0000

    feat: emit user agent (for auth request clients)

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,23 +6,23 @@ import { LiveSocket } from "phoenix_live_view";
 import Hooks from "./hooks";
 import topbar from "../vendor/topbar";
 
-let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
-let liveSocket = new LiveSocket("/live", Socket, {
+const csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content");
+const liveSocket = new LiveSocket("/live", Socket, {
     hooks: Hooks,
     params: {
         _csrf_token: csrfToken,
-        states: Hooks.SaveState.getAllStates()
+        states: Hooks.SaveState.getAllStates(),
+        agent: window.navigator && window.navigator.userAgent || ""
     }
-})
+});
 
 topbar.config({
     barColors: { 0: "#29d" },
     shadowColor: "rgba(0, 0, 0, .3)"
-})
-window.addEventListener("phx:page-loading-start", info => topbar.show())
-window.addEventListener("phx:page-loading-stop", info => topbar.hide())
+});
+window.addEventListener("phx:page-loading-start", info => topbar.show());
+window.addEventListener("phx:page-loading-stop", info => topbar.hide());
 
-liveSocket.connect()
-liveSocket.disableLatencySim()
-window.liveSocket = liveSocket
-
+liveSocket.connect();
+liveSocket.disableLatencySim();
+window.liveSocket = liveSocket;
